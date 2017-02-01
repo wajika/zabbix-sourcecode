d4a65d1d77d32487bd86f795e3a277f5874ddcb0
Viktors Tjarve <viktors.tjarve@zabbix.com>
2017-02-01 15:17:51 +0000

    ........S. [ZBX-11738] fixed action operation execution if problem is resolved while escalation is in progress

--- src/zabbix_server/escalator/escalator.c
+++ src/zabbix_server/escalator/escalator.c
@@ -2121,29 +2121,18 @@ static int	process_escalations(int now, int *nextcheck, unsigned int escalation_
 		/* Execute operations and recovery operations, mark changes in 'diffs' for batch saving in DB below. */
 		diff = escalation_create_diff(&escalation);
 
-		if (0 != escalation.r_eventid)
+		if (ESCALATION_STATUS_ACTIVE == escalation.status)
 		{
-			escalation_recover(&escalation, &action, &event, &r_event);
-		}
-		else if (escalation.nextcheck <= now)
-		{
-			if (ESCALATION_STATUS_ACTIVE == escalation.status)
-			{
+			if (escalation.nextcheck <= now && (0 == escalation.r_eventid || 0 == escalation.esc_step))
 				escalation_execute(&escalation, &action, &event);
-			}
-			else if (ESCALATION_STATUS_SLEEP == escalation.status)
-			{
-				escalation.nextcheck = time(NULL) + SEC_PER_MIN;
-			}
-			else
-			{
-				THIS_SHOULD_NEVER_HAPPEN;
-			}
 		}
+		else if (ESCALATION_STATUS_SLEEP == escalation.status)
+			escalation.nextcheck = time(NULL) + SEC_PER_MIN;
 		else
-		{
 			THIS_SHOULD_NEVER_HAPPEN;
-		}
+
+		if (0 != escalation.r_eventid)
+			escalation_recover(&escalation, &action, &event, &r_event);
 
 		escalation_update_diff(&escalation, diff);
 		zbx_vector_ptr_append(&diffs, diff);
