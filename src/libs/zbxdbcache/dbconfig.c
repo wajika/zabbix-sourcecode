0c0cd1c486c7fd3bde6714243feac7c889ed60cd
Glebs Ivanovskis <git-no-reply@zabbix.com>
2017-08-22 15:11:12 +0000

    .......PS. [ZBX-12549] inverted condition

--- src/libs/zbxdbcache/dbconfig.c
+++ src/libs/zbxdbcache/dbconfig.c
@@ -405,13 +405,11 @@ static void	DCitem_poller_type_update(ZBX_DC_ITEM *dc_item, const ZBX_DC_HOST *d
 
 	poller_type = poller_by_item(dc_item->type, dc_item->key);
 
-	if (ZBX_POLLER_TYPE_UNREACHABLE == dc_item->poller_type &&
-			(ZBX_POLLER_TYPE_NORMAL == poller_type || ZBX_POLLER_TYPE_JAVA == poller_type))
+	if (ZBX_POLLER_TYPE_UNREACHABLE != dc_item->poller_type ||
+			(ZBX_POLLER_TYPE_NORMAL != poller_type && ZBX_POLLER_TYPE_JAVA != poller_type))
 	{
-		return;
+		dc_item->poller_type = poller_type;
 	}
-
-	dc_item->poller_type = poller_type;
 }
 
 static int	DCget_disable_until(const ZBX_DC_ITEM *item, const ZBX_DC_HOST *host)
