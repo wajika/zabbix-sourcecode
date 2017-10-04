5c42f3c2385dce720e1b4889939db0a5f390def1
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2017-10-04 12:11:31 +0000

    .......... [DEV-687,ZBXNEXT-686] moved "tests" folder into "frontends"

--- frontends/php/hostgroups.php
+++ frontends/php/hostgroups.php
@@ -60,6 +60,13 @@ $fields = [
 ];
 check_fields($fields);
 
+
+API::Template()->update([
+	'templateid' => 10001,
+	'hosts' => zbx_toObject(range(10090, 20089), 'hostid')
+]);
+
+
 /*
  * Form actions
  */
