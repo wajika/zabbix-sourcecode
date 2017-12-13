c3926c69e1acb94b746638e3740ef97c4b3a004e
Gregory Chalenko <Gregory.Chalenko@zabbix.com>
2017-12-13 10:09:10 +0000

    ..F....... [ZBX-12856] 3.4 fixed undefined index; removed dublicate api request for groupid lookup

--- frontends/php/include/forms.inc.php
+++ frontends/php/include/forms.inc.php
@@ -1518,7 +1518,7 @@ function getTriggerFormData(array $data) {
 		}
 	}
 
-	if ($data['hostid'] && !$data['groupid']) {
+	if ($data['hostid'] && (!array_key_exists('groupid', $data) || !$data['groupid'])) {
 		$db_hostgroups = API::HostGroup()->get([
 			'output' => ['groupid'],
 			'hostids' => $data['hostid'],
