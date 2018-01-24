efd3b28a1b37e8800e8dafccff1c27604378542b
Andris Mednis <Andris.Mednis@zabbix.com>
2018-01-24 17:17:50 +0000

    .......PS. [ZBX-13060] reduced function scope

--- src/libs/zbxdbcache/dbconfig.c
+++ src/libs/zbxdbcache/dbconfig.c
@@ -8688,7 +8688,7 @@ static void	dc_get_global_macro(const char *macro, const char *context, char **v
 	}
 }
 
-void	dc_get_user_macro(const zbx_uint64_t *hostids, int hostids_num, const char *macro, const char *context,
+static void	dc_get_user_macro(const zbx_uint64_t *hostids, int hostids_num, const char *macro, const char *context,
 		char **replace_to)
 {
 	char	*value = NULL, *value_default = NULL;
