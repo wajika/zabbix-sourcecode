c0bd8f43b6e343200492f819a72e48e7ccf20268
Andris Zeila <andris.zeila@zabbix.com>
2018-03-19 14:33:25 +0000

    ........S. [ZBX-13611] added lld rule state/error update in configruation cache when processing supported checks

--- src/libs/zbxdbhigh/lld.c
+++ src/libs/zbxdbhigh/lld.c
@@ -573,8 +573,12 @@ void	lld_process_discovery_rule(zbx_uint64_t lld_ruleid, const char *value, cons
 	const char		*sql_start = "update items set ", *sql_continue = ",";
 	lld_filter_t		filter;
 	time_t			now;
+	zbx_item_diff_t		lld_rule_diff;
 
 	zabbix_log(LOG_LEVEL_DEBUG, "In %s() itemid:" ZBX_FS_UI64, __function_name, lld_ruleid);
+
+	lld_rule_diff.itemid = lld_ruleid;
+	lld_rule_diff.flags = ZBX_FLAGS_ITEM_DIFF_UNSET;
 
 	if (FAIL == DCconfig_lock_lld_rule(lld_ruleid))
 	{
@@ -673,6 +677,9 @@ void	lld_process_discovery_rule(zbx_uint64_t lld_ruleid, const char *value, cons
 
 		zbx_snprintf_alloc(&sql, &sql_alloc, &sql_offset, "%sstate=%d", sql_start, ITEM_STATE_NORMAL);
 		sql_start = sql_continue;
+
+		lld_rule_diff.state = ITEM_STATE_NORMAL;
+		lld_rule_diff.flags |= ZBX_FLAGS_ITEM_DIFF_UPDATE_STATE;
 	}
 
 	/* add informative warning to the error message about lack of data for macros used in filter */
@@ -687,18 +694,28 @@ error:
 		sql_start = sql_continue;
 
 		zbx_free(error_esc);
+
+		lld_rule_diff.error = error;
+		lld_rule_diff.flags |= ZBX_FLAGS_ITEM_DIFF_UPDATE_ERROR;
 	}
 
 	if (sql_start == sql_continue)
 	{
 		zbx_snprintf_alloc(&sql, &sql_alloc, &sql_offset, " where itemid=" ZBX_FS_UI64, lld_ruleid);
-
-		DBbegin();
-
 		DBexecute("%s", sql);
-
-		DBcommit();
 	}
+
+	if (ZBX_FLAGS_ITEM_DIFF_UNSET != lld_rule_diff.flags)
+	{
+		zbx_vector_ptr_t	diffs;
+
+		zbx_vector_ptr_create(&diffs);
+		zbx_vector_ptr_append(&diffs, &lld_rule_diff);
+		DCconfig_items_apply_changes(&diffs);
+		zbx_vector_ptr_destroy(&diffs);
+	}
+
+
 clean:
 	DCconfig_unlock_lld_rule(lld_ruleid);
 
