5527cf5d010a4becec0a94b2f78cf262ab5fdfb4
Natalja Karpinska <natalja.karpinska@zabbix.com>
2018-06-05 06:29:56 +0000

    .......... [ZBXNEXT-826] updated release version [3.4.11rc1]; updated API version [3.4.11]

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -24,7 +24,7 @@ class GeneralInformation
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
 	static final String REVISION_DATE = "4 June 2018";
 	static final String REVISION = "{ZABBIX_REVISION}";
-	static final String VERSION = "3.4.10";
+	static final String VERSION = "3.4.11rc1";
 
 	static void printVersion()
 	{
