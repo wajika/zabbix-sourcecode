b232b23fc30fff7a652f1a76a5cafe6655823d68
Gregory Chalenko <gregory.chalenko@zabbix.com>
2019-07-05 12:20:58 +0300

    ..F....... [DEV-1115] mesh mouse over detection draft

--- frontends/php/js/widget.3dcanvas.js
+++ frontends/php/js/widget.3dcanvas.js
@@ -23,6 +23,7 @@
 var __log = console['log'];
 
 var widgets_canvas = {},
+	raycaster = new THREE.Raycaster(),
 	geometry = {
 		sphere: new THREE.SphereGeometry(1, 70, 70, 0, Math.PI * 2, 0, Math.PI * 2),
 		icosahedron: new THREE.IcosahedronGeometry(1, 5)
@@ -32,11 +33,17 @@ var widgets_canvas = {},
 		green: new THREE.MeshStandardMaterial({color: 0x08ee08, flatShading: true}),
 		blue: new THREE.MeshStandardMaterial({color: 0x0000ff, flatShading: true}),
 		inner_sphere: new THREE.MeshStandardMaterial({color: 0x2020ff, transparent: true, opacity: 0.1})
-	};
+	},
+	INTERSECTED;// TODO: move to widgets_canvas object
 
 $.subscribe('init.widget.3dcanvas', initWidget3dCanvasHandler);
 animate();
 
+/**
+ * Initialize canvas object.
+ *
+ * @param {Object} ev        jQuery event object.
+ */
 function initWidget3dCanvasHandler(ev) {
 	__log('init.widget.3dcanvas', arguments);
 	// find by unique id passed in ev.widgetid field
@@ -44,8 +51,37 @@ function initWidget3dCanvasHandler(ev) {
 	var widgetid = '1234';
 
 	widgets_canvas[widgetid] = init(container);
+	widgets_canvas[widgetid].mouse = {x: 0, y:0};
 	fillScene(widgets_canvas[widgetid], ev.scene, pointsDistributionSphere);
+
+	container.on('mousemove', {widgetid: widgetid}, containerMouseMoveHandler);
 }
+
+/**
+ * Updates widget mouse position to scene.
+ *
+ * @param {Object} ev         jQuery event object.
+ */
+function containerMouseMoveHandler(ev) {
+	if (!ev.data || !ev.data.widgetid) {
+		return;
+	}
+
+	ev.preventDefault();
+
+	var widgetid = ev.data.widgetid,
+		container = $(ev.target),
+		rect = ev.target.getBoundingClientRect();
+
+	widgets_canvas[widgetid].mouse = {
+		x: ((ev.clientX - rect.left) / container.width()) * 2 - 1,
+		y: - ((ev.clientY - rect.top) / container.height()) * 2 + 1
+	}
+	
+	//console.log(widgets_canvas[widgetid].mouse, [ev.clientX - rect.left, ev.clientY - rect.top, container]);
+	// console.log('camera', widgets_canvas[widgetid].renderer.getSize());
+	// console.log('container', [container.width(), container.height()]);
+  }
 
 /**
  * Initialize three.js scene.
@@ -53,7 +89,7 @@ function initWidget3dCanvasHandler(ev) {
  * @param {Object} container   Dom element, 3d canvas container.
  */
 function init(container) {
-	var camera = new THREE.PerspectiveCamera(50, container.width() / container.height(), 0.1, 1000);
+	var camera = new THREE.PerspectiveCamera(70, container.width() / container.height(), 0.1, 1000);
 	// var camera = new THREE.OrthographicCamera(container.width() / -2, container.width() / 2, container.height() / 2,
 	// 	container.height() / -2, 1, 1000
 	// );
@@ -83,11 +119,18 @@ function init(container) {
 	var sprite_glow = new THREE.Sprite(sprite_glow_material);
 	sprite_glow.scale.set(7, 7, 1.0);
 
+
+	// Camera auto rotation
+	controls.target = new THREE.Vector3(3, 7, 0);
+    controls.update();
+    controls.autoRotate = true;
+    controls.autoRotateSpeed = 3.0;
+
 	return {
+		controls: controls,
 		scene: scene,
 		camera: camera,
 		renderer: renderer,
-		// glow_material: glow,
 		sprite_glow: sprite_glow
 	}
 }
@@ -122,7 +165,7 @@ function fillScene(scene, data, points) {
 			__log(`old parent ${elm.id}`, processed[elm.id]);
 		}
 		else {
-			var mesh = new THREE.Mesh(geometry[elm.geometry], material.blue);
+			var mesh = new THREE.Mesh(geometry[elm.geometry], material.blue.clone());
 			distance = 8;
 			mesh.position.set(x,y,z);
 			mesh.add(scene.sprite_glow.clone());
@@ -156,7 +199,7 @@ function fillScene(scene, data, points) {
 			var elm = data.elements.filter(elm => {
 				return elm.id === children[i].child;
 			}).pop();
-			var mesh = new THREE.Mesh(geometry[elm.geometry], material.blue);
+			var mesh = new THREE.Mesh(geometry[elm.geometry], material.blue.clone());
 			mesh.position.set(x + pos[0], y + pos[1], z + pos[2]);
 			mesh.add(scene.sprite_glow.clone());
 			scene.scene.add(mesh);
@@ -186,18 +229,49 @@ function fillScene(scene, data, points) {
 function animate() {
 	requestAnimationFrame(animate);
 
-	var speed = Date.now() * 0.0002;
-
 	Object.values(widgets_canvas).each(scene => {
+		scene.controls.update();
 		scene.renderer.render(scene.scene, scene.camera);
+		scene.camera.updateMatrixWorld();
 
-		scene.camera.position.x = Math.cos(speed) * 10;
-		scene.camera.position.z = Math.sin(speed) * 10;
-		scene.camera.position.y = 120 + Math.sin(speed) * 20;
-
-		scene.camera.lookAt(scene.scene.position);
+		raycaster.setFromCamera(scene.mouse, scene.camera);
+		markMouseIntersection(scene);
 	});
 };
+
+/**
+ * Find 3d objects hovered by mouse.
+ *
+ * @param {Object} scene     Scene object
+ */
+function markMouseIntersection(scene)
+{
+	var tick = Date.now() * 0.0002;
+	var intersects = raycaster.intersectObjects(scene.scene.children);
+
+	if (intersects.length > 0) {
+		scene.controls.autoRotate = false;
+
+		__log('intersected', intersects);
+		if ( INTERSECTED != intersects[ 0 ].object ) {
+			if ( INTERSECTED ) INTERSECTED.material.color.setHex(INTERSECTED.currentHex);
+			INTERSECTED = intersects[ 0 ].object;
+			INTERSECTED.currentHex = INTERSECTED.material.color.getHex();
+			
+			if (INTERSECTED.children) {
+				INTERSECTED.material.color.setHex(0xffffff);
+			}
+			else {
+				INTERSECTED.material.color.setHex(0xffffff);
+			}
+		}
+	} else {
+		if ( INTERSECTED ) INTERSECTED.material.color.setHex(INTERSECTED.currentHex);
+		INTERSECTED = null;
+
+		scene.controls.autoRotate = true;
+	}
+}
 
 /**
  * Generate ${count} coordinates distributing over sphere.
