befdb43d2665041d1e175ac10213e41b9f8c9d87
Larisa Grigorjeva <larisa.grigorjeva@zabbix.com>
2019-08-20 18:17:22 +0300

    .......... [DEV-958] added new checks for PrototypesMassUpdate

--- frontends/php/tests/selenium/testPageItemPrototypesMassUpdate.php
+++ frontends/php/tests/selenium/testPageItemPrototypesMassUpdate.php
@@ -29,7 +29,9 @@ class testPageItemPrototypesMassUpdate extends CWebTest{
 
 	const DISCOVERY_RULE_ID = 33800;
 
-	public function getChangeItemTypeData() {
+	public $host_name = 'Simple form test host';
+
+	public function getChangeItemProtoypeData() {
 		return [
 			[
 				[
@@ -45,8 +47,8 @@ class testPageItemPrototypesMassUpdate extends CWebTest{
 						'Update interval' => [
 							'Delay' => '90s',
 							'Custom intervals' => [
-								['type' => 'Flexible', 'interval' => '60s', 'period' => '2-5,3:00-17:00'],
-								['type' => 'Scheduling', 'interval' => 'wd3-4h1-15']
+								['Type' => 'Flexible', 'Interval' => '60s', 'Period' => '2-5,3:00-17:00'],
+								['Type' => 'Scheduling', 'Interval' => 'wd3-4h1-15']
 							]
 						],
 						'History storage period' => [
@@ -79,12 +81,13 @@ class testPageItemPrototypesMassUpdate extends CWebTest{
 			[
 				[
 					'names'=> [
-						'Prototype for mass update 3  (SNMPv1 => SNMPv3)',
-						'Prototype for mass update 4  (SNMPv1 => SNMPv3)'
+						'Prototype for mass update 3 (SNMPv1 => SNMPv3)',
+						'Prototype for mass update 4 (SNMPv1 => SNMPv3)'
 					],
 					'change' => [
+						'Type' => 'SNMPv3 agent',
 						'Context name' => 'New Context name',
-						'Security name' => ' New Security name',
+						'Security name' => 'New Security name',
 						'Security level'=> 'authPriv',
 						'Authentication protocol'=> 'SHA',
 						'Authentication passphrase' => 'New_passphrase',
@@ -99,10 +102,11 @@ class testPageItemPrototypesMassUpdate extends CWebTest{
 			[
 				[
 					'names'=> [
-						'Prototype for mass update 5  (SNMPv2 => SNMPv1)',
-						'Prototype for mass update 6  (SNMPv2 => SNMPv1)'
+						'Prototype for mass update 5 (SNMPv2 => SNMPv1)',
+						'Prototype for mass update 6 (SNMPv2 => SNMPv1)'
 					],
 					'change' => [
+						'Type' => 'SNMPv1 agent',
 						'SNMP community' => 'New Test Community',
 					]
 				]
@@ -183,8 +187,8 @@ class testPageItemPrototypesMassUpdate extends CWebTest{
 						'Request body type' => 'JSON data',
 						'Request body' => '{"html":"<body>1</body>"}',
 						'Headers' => [
-							['name' => 'name1', 'value' => 'value1'],
-							['name' => 'name2', 'value' => 'value2']
+							['Name' => 'header1', 'Value' => '1'],
+							['Name' => 'header2', 'Value' => '2']
 						],
 						'Enable trapping' => true
 					]
@@ -207,9 +211,9 @@ class testPageItemPrototypesMassUpdate extends CWebTest{
 	}
 
 	/**
-	 * @dataProvider getChangeItemTypeData
+	 * @dataProvider getChangeItemProtoypeData
 	 */
-	public function testPageItemPrototypesMassUpdate_ChangeItemType($data) {
+	public function testPageItemPrototypesMassUpdate_ChangeItemProtoype($data) {
 		$this->page->login()->open('disc_prototypes.php?parent_discoveryid='.self::DISCOVERY_RULE_ID);
 		// Get item table.
 		$table = $this->query('xpath://form[@name="items"]/table[@class="list-table"]')->asTable()->one();
@@ -224,7 +228,7 @@ class testPageItemPrototypesMassUpdate extends CWebTest{
 			// Click on a label to show input control.
 			$form->getLabel($field)->click();
 			// Set field value.
-			if ($field === 'Host interface'){
+			if ($field === 'Host interface') {
 				$form->query('id:interfaceid')->asDropdown()->one()->select($value);
 			}
 			elseif ($field === 'Update interval'){
@@ -232,7 +236,7 @@ class testPageItemPrototypesMassUpdate extends CWebTest{
 				$delay = $container_table->getRow(0)->getColumn(1);
 				$delay->query('id:delay')->one()->fill($value['Delay']);
 
-				$intervals_table= $container_table->getRow(1)->getColumn(1)->query('id:custom_intervals')
+				$intervals_table = $container_table->getRow(1)->getColumn(1)->query('id:custom_intervals')
 					->asMultifieldTable([
 						'mapping' => [
 							[
@@ -256,13 +260,13 @@ class testPageItemPrototypesMassUpdate extends CWebTest{
 					[
 						'action' => USER_ACTION_UPDATE,
 						'index' => 0,
-						'Type' => $value['Custom intervals'][0]['type'],
-						'Interval' => $value['Custom intervals'][0]['interval'],
-						'Period' => $value['Custom intervals'][0]['period']
+						'Type' => $value['Custom intervals'][0]['Type'],
+						'Interval' => $value['Custom intervals'][0]['Interval'],
+						'Period' => $value['Custom intervals'][0]['Period']
 					],
 					[
-						'Type' => $value['Custom intervals'][1]['type'],
-						'Interval' => $value['Custom intervals'][1]['interval']
+						'Type' => $value['Custom intervals'][1]['Type'],
+						'Interval' => $value['Custom intervals'][1]['Interval']
 					]
 				]);
 			}
@@ -279,17 +283,17 @@ class testPageItemPrototypesMassUpdate extends CWebTest{
 				$app_container->query('class:input')->one()->fill($value['application']);
 				$app_container->query('class:multiselect-suggest')->waitUntilVisible()->one()->click();
 			}
-			elseif ($field === 'Show value'){
+			elseif ($field === 'Show value') {
 				$form->query('id:valuemapid')->asDropdown()->one()->select($value);
 			}
 			// Rewrite after DEV-1257 is done.
-			elseif ($field === 'Master item'){
+			elseif ($field === 'Master item') {
 				$container = $form->query('id:master_item')->one();
 				$container->query('button:'.$value['button'])->one()->click();
 				$dialog = COverlayDialogElement::find()->one();
 				$dialog->query('link:'.$value['item'])->waitUntilVisible()->one()->click();
 			}
-			elseif ($field === 'Headers'){
+			elseif ($field === 'Headers') {
 				$headers_table = $form->query('id:headers_pairs')->asMultifieldTable([
 						'mapping' => [
 							[
@@ -308,12 +312,12 @@ class testPageItemPrototypesMassUpdate extends CWebTest{
 					[
 						'action' => USER_ACTION_UPDATE,
 						'index' => 0,
-						'Name' => $value[0]['name'],
-						'Value' => $value[0]['value']
+						'Name' => $value[0]['Name'],
+						'Value' => $value[0]['Value']
 					],
 					[
-						'Name' => $value[1]['name'],
-						'Value' => $value[1]['value']
+						'Name' => $value[1]['Name'],
+						'Value' => $value[1]['Value']
 					]
 				]);
 			}
@@ -326,5 +330,190 @@ class testPageItemPrototypesMassUpdate extends CWebTest{
 		$message = CMessageElement::find()->one();
 		$this->assertTrue($message->isGood());
 		$this->assertEquals('Item prototypes updated', $message->getTitle());
+
+		// Check that fields are saved for each item.
+		foreach ($data['names'] as $name) {
+			$this->page->open('disc_prototypes.php?parent_discoveryid='.self::DISCOVERY_RULE_ID);
+			// Open modified items.
+			if(array_key_exists('Type', $data['change']) && $data['change']['Type'] === 'Dependent item'){
+				$cell_name = $data['change']['Master item']['item'].': '.$name;
+				$table->findRow('Name', $cell_name)->query('link:'.$name)->one()->click();
+			}
+			else{
+				$table->findRow('Name', $name)->query('link:'.$name)->one()->click();
+			}
+
+			$form = $this->query('name:itemForm')->waitUntilPresent()->asForm()->one();
+
+
+			foreach ($data['change'] as $field => $value) {
+				switch ($field) {
+					case 'Type':
+					case 'Host interface':
+					case 'Type of information':
+					case 'Units':
+					case 'Description':
+					case 'Context name':
+					case 'Security name':
+					case 'Security level':
+					case 'Authentication protocol':
+					case 'Authentication passphrase':
+					case 'Privacy protocol':
+					case 'Privacy passphrase':
+					case 'Port':
+					case 'Type of information':
+					case 'Log time format':
+					case 'SNMP community':
+					case 'Allowed hosts':
+					case 'JMX endpoint':
+					case 'User name':
+					case 'Password':
+					case 'Authentication method':
+					case 'Public key file':
+					case 'Private key file':
+					case 'URL':
+					case 'Request body type':
+					case 'Request body':
+					case 'Enable trapping':
+						$this->assertEquals($value, $form->getField($field)->getValue());
+						break;
+					case 'Create enabled':
+						$checkbox = $form->getField($field)->getValue();
+						if ($checkbox === true) {
+							$checkbox = 'Enabled';
+						}
+						else {
+							$checkbox = 'Disabled';
+						}
+						$this->assertEquals($value, $checkbox);
+						break;
+					case 'Update interval':
+						$update_interval = $form->getField($field)->getValue();
+						$this->assertEquals($value['Delay'], $update_interval);
+
+						$custom_intervals_table = $form->query('id:delayFlexTable')
+							->asMultifieldTable([
+								'mapping' => [
+									[
+										'name' => 'Type',
+										'selector' => 'class:radio-list-control',
+										'class' => 'CSegmentedRadioElement'
+									],
+									[
+										'name' => 'Interval',
+										'selector' => 'xpath:./input',
+										'class' => 'CElement'
+									],
+									[
+										'name' => 'Period',
+										'selector' => 'xpath:./input',
+										'class' => 'CElement'
+									]
+								]
+							])->one();
+						$this->assertEquals($value['Custom intervals'], $custom_intervals_table->getValue());
+						break;
+					case 'History storage period':
+					case 'Trend storage period':
+						$storage_container = $form->getFieldContainer($field);
+						$storage_radio_value = $storage_container->query('id:'.$value['case'].'_mode')->one()->asSegmentedRadio()->getValue();
+						$this->assertEquals($value['type'], $storage_radio_value);
+						if(array_key_exists('period', $value)){
+							$storage_period_value = $storage_container->query('xpath:.//input[@type="text"]')->one()->getValue();
+							$this->assertEquals($value['period'], $storage_period_value);
+						}
+						break;
+					case 'Show value':
+							$show_value = $form->query('id:valuemapid')->asDropdown()->one()->getValue();
+							$this->assertEquals($value, $show_value);
+						break;
+					case 'Applications':
+					case 'Application prototypes':
+						$this->assertEquals($value['application'], $form->getField($field)->getValue());
+						break;
+					case 'Master item':
+						$master_item_container = $form->getFieldContainer($field);
+						$master_item_value = $master_item_container->query('id:master_itemname')->one()->getValue();
+						$this->assertEquals($this->host_name.': '.$value['item'], $master_item_value);
+						break;
+					case 'Headers':
+						$headers_table = $form->query('id:headers_pairs')
+							->asMultifieldTable([
+								'mapping' => [
+									[
+										'name' => 'Name',
+										'selector' => 'xpath:./input',
+										'class' => 'CElement'
+									],
+									[
+										'name' => 'Value',
+										'selector' => 'xpath:./input',
+										'class' => 'CElement'
+									]
+								]
+							])->one();
+						$this->assertEquals($value, $headers_table->getValue());
+						break;
+				}
+			}
+		}
+	}
+
+
+		public function ChangePreprocessing() {
+		return [
+			[
+				[
+					'names'=> [
+						'Prototype for mass update 1 (active agent => agent)',
+						'Prototype for mass update 2 (active agent => agent)'
+					],
+					'change' => [
+						'Type' => 'Zabbix agent',
+						'Host interface' => '127.0.0.2 : 10099',
+						'Type of information'=> 'Numeric (float)',
+						'Units'=> '$',
+						'Update interval' => [
+							'Delay' => '90s',
+							'Custom intervals' => [
+								['Type' => 'Flexible', 'Interval' => '60s', 'Period' => '2-5,3:00-17:00'],
+								['Type' => 'Scheduling', 'Interval' => 'wd3-4h1-15']
+							]
+						],
+						'History storage period' => [
+							'type' => 'Do not keep history',
+							'case' => 'history'
+						],
+						'Trend storage period' => [
+							'type' => 'Storage period',
+							'period' => '400d',
+							'case' => 'trends'
+						],
+						'Show value' => 'TruthValue',
+						'Applications' => [
+							'action' => 'Add',
+							'query' => 'applications',
+							'radio'=> 'app',
+							'application' => 'New application'
+						],
+						'Application prototypes' => [
+							'action' => 'Add',
+							'query' => 'application_prototypes',
+							'radio' => 'app_prot',
+							'application' => 'New application prototype'
+						],
+						'Description' => 'New test description',
+						'Create enabled' => 'Disabled'
+					]
+				]
+			]
+		];
+	}
+
+	/**
+	 * @dataProvider getChangePreprocessingData
+	 */
+	public function testPageItemPrototypesMassUpdate_ChangePreprocessing($data) {
+
 	}
 }
