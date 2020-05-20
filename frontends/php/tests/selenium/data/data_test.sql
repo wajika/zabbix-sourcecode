3a8b6db95b2818b1e698c33b8dfc866d7951db32
Larisa Grigorjeva <larisa.grigorjeva@zabbix.com>
2020-05-20 14:32:29 +0300

    .......... [DEV-1380] fixed test data

--- frontends/php/tests/selenium/data/data_test.sql
+++ frontends/php/tests/selenium/data/data_test.sql
@@ -2339,7 +2339,7 @@ INSERT INTO dashboard (dashboardid, name, userid, private) VALUES (105, 'Dashboa
 INSERT INTO widget (widgetid, dashboardid, type, name, x, y, width, height) VALUES (199, 105, 'graphprototype', 'Graph prototype widget for update', 0, 0, 16, 5);
 INSERT INTO widget_field (widget_fieldid, widgetid, type, name, value_graphid) VALUES (90044, 199, 7, 'graphid', 600003);
 
-INSERT INTO widget (widgetid, dashboardid, type, name, x, y, width, height) VALUES (200, 105, 'graphprototype', 'Graph prototype widget for delete', 0, 0, 16, 5);
+INSERT INTO widget (widgetid, dashboardid, type, name, x, y, width, height) VALUES (200, 105, 'graphprototype', 'Graph prototype widget for delete', 0, 5, 16, 5);
 INSERT INTO widget_field (widget_fieldid, widgetid, type, name, value_graphid) VALUES (90045, 200, 7, 'graphid', 600002);
 
 INSERT INTO dashboard (dashboardid, name, userid, private) VALUES (106, 'Dashboard for Sceenshoting Graph Prototype widgets', 1, 1);
